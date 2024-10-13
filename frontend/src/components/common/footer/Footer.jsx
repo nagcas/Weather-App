5e41e2c1ee6ffaec217dc7901e080d443cae9be1
Gianluca Chiaravalloti <studio.nagcas@outlook.it>
2024-10-14 01:12:24 +0200

    add menu footer

--- frontend/src/components/common/footer/Footer.jsx
+++ frontend/src/components/common/footer/Footer.jsx
@@ -1,8 +1,22 @@
+import "./Footer.css";
+
+import { Link } from 'react-router-dom';
+
 function Footer() {
+
+  const currentYear = new Date().getFullYear();
+
   return (
-    <>
-      <p>footer</p>
-    </>
+    <footer className="py-3 my-4">
+    <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+      <li className="nav__item"><Link to="/" className="nav__link px-2 text-body-secondary">Home</Link></li>
+      <li className="nav__item"><Link to="/current-weather" className="nav__link px-2 text-body-secondary">Current Weather</Link></li>
+      <li className="nav__item"><Link to="/forecast" className="nav__link px-2 text-body-secondary">Forecast</Link></li>
+      <li className="nav__item"><Link to="/favorites" className="nav__link px-2 text-body-secondary">Favorites</Link></li>
+      <li className="nav__item"><Link to="about" className="nav__link px-2 text-body-secondary">About</Link></li>
+    </ul>
+    <p className="text-center text-body-secondary">© {currentYear} Weather App</p>
+  </footer>
   );
 };
 
