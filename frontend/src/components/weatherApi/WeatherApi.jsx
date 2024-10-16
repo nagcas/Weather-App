9073b1eed98d33d8733f22d2bc82289308d735e9
Gianluca Chiaravalloti <studio.nagcas@outlook.it>
2024-10-16 11:08:11 +0200

    update icon weather

--- frontend/src/components/weatherApi/WeatherApi.jsx
+++ frontend/src/components/weatherApi/WeatherApi.jsx
@@ -1,6 +1,7 @@
+import "./WeatherApi.css";
 import { useState } from "react";
 import { useFetch } from "../../modules/useFetch.js";
-import { Container } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
 
 function WeatherApi() {
 
@@ -10,15 +11,27 @@ function WeatherApi() {
   const URL_API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   const { data, loading, error } = useFetch(URL_API);
 
+  const icon = data?.weather?.[0]?.icon ? `https://openweathermap.org/img/wn/${data.weather[0]?.icon}@4x.png` : null;
+
   console.log(data);
 
   return (
     <Container className="mt-4">
       {error && <p>Error: {error}</p>}
       {loading && <p>Loading...</p>}
-      <p>City: <span className="fw-bold">{data.name}</span></p>
-      <p>Temp. <span className="fw-bold">{data.main?.temp} °C</span> - Temp. Min <span className="fw-bold">{data.main?.temp_min} °C</span>  - Temp. Max <span className="fw-bold">{data.main?.temp_max} °C</span></p>
-      <p>Pressure: <span className="fw-bold">{data.main?.pressure} mmHg</span> - Humidity: <span className="fw-bold">{data.main?.humidity} %</span></p>
+      <div className="content__weather">
+        <Row className="d-flex justify-content-center">
+          <Col>
+            <Image src={icon} alt="weather icon" />
+          </Col>
+          <Col>
+            <p>City: <span className="fw-bold">{data.name}</span></p>
+            <p>Temp. <span className="fw-bold">{data.main?.temp} °C</span> - Temp. Min <span className="fw-bold">{data.main?.temp_min} °C</span>  - Temp. Max <span className="fw-bold">{data.main?.temp_max} °C</span></p>
+            <p>Pressure: <span className="fw-bold">{data.main?.pressure} mmHg</span> - Humidity: <span className="fw-bold">{data.main?.humidity} %</span></p>
+            <p>Wind: <span>{data.wind?.speed} meter/sec</span> - Deg: <span>{data.wind?.deg} degrees</span> - Gust: <span>{data.wind?.gust} meter/sec</span></p>
+          </Col>
+        </Row>
+      </div>
     </Container>
   );
 };
