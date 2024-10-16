905b4d9fd9afde534fb8b43997e6745cd650dfd6
Gianluca Chiaravalloti <studio.nagcas@outlook.it>
2024-10-16 21:34:14 +0200

    change  require con import

--- backend/src/controllers/getFavoriteCities.js
+++ backend/src/controllers/getFavoriteCities.js
@@ -1,29 +1,23 @@
-const userModel = require("../models/user");
+import userModel from "../models/user.js";
 
-const getFavoriteCities = async(req,res) =>{
-    //as this controller will be called after the request has passed auth middleware so the request will contain userid already and we know the user is authenticated
+const getFavoriteCities = async (req, res) => {
+  //as this controller will be called after the request has passed auth middleware so the request will contain userid already and we know the user is authenticated
 
-    const { userId } = req;
+  const { userId } = req;
+  try {
+    const user = await userModel.findById(userId);
 
-    try {
-        const user = await userModel.findById(userId);
-
-
-
-        if( !user ||!user.favoriteCities || user.favoriteCities.length == 0 )
-        {
-
-             return res.status(200).json({message:"No favorite cities found"});
-
-        }
-
-
-        return res.json({list:user.favoriteCities}).status(200);
-        
-    } catch (error) {
-        console.error("error occured in getting favorite cities ", error);
-        return res.status(500).json({message:"error occured in getting cities"});
+    if (!user || !user.favoriteCities || user.favoriteCities.length == 0) {
+      return res.status(200).json({ message: "No favorite cities found." });
     }
-}
 
-module.exports = getFavoriteCities;
+    return res.json({ list: user.favoriteCities }).status(200);
+  } catch (error) {
+    console.error("Error occured in getting favorite cities: ", error);
+    return res
+      .status(500)
+      .json({ message: "Error occured in getting cities." });
+  }
+};
+
+export default getFavoriteCities;
