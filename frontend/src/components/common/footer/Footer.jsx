7b76b863706bc2c76606cdb2824b6a5865d551f2
Gianluca Chiaravalloti <studio.nagcas@outlook.it>
2024-10-20 00:00:58 +0200

    add verify loggedin and view menu favorites city

--- frontend/src/components/common/footer/Footer.jsx
+++ frontend/src/components/common/footer/Footer.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import "./Footer.css";
 
 import { Link } from 'react-router-dom';
+import { Context } from "../../../modules/Context";
 
 function Footer() {
+
+  const { isLoggedIn } = useContext(Context);
 
   const currentYear = new Date().getFullYear();
 
@@ -12,7 +16,9 @@ function Footer() {
       <li className="nav__item"><Link to="/" className="nav__link px-2">Home</Link></li>
       <li className="nav__item"><Link to="/default-weather" className="nav__link px-2">Default Cities Weather</Link></li>
       <li className="nav__item"><Link to="/forecast" className="nav__link px-2">Forecast</Link></li>
-      <li className="nav__item"><Link to="/favorites" className="nav__link px-2">Favorites</Link></li>
+      {isLoggedIn && (
+        <li className="nav__item"><Link to="/favorites" className="nav__link px-2">Favorites City</Link></li>
+      )}
       <li className="nav__item"><Link to="about" className="nav__link px-2">About</Link></li>
     </ul>
     <p>Created with ❤️ during <a className="link__hack" href="https://hacktoberfest.com/" target="_blank" rel="noopener noreferrer">Hacktoberfest</a> 2024</p>
