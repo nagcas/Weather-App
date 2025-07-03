dcf0db3bcf60c0cff8850538bf2061e7d40a183f
nagcas <studio.nagcas@outlook.it>
2025-07-03 02:18:47 +0200

    refactor: improve code formatting and consistency in loginController.js

--- backend/src/controllers/loginController.js
+++ backend/src/controllers/loginController.js
@@ -1,41 +1,48 @@
-import userModel from "../models/user.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import User from '../models/user.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
 // Configuring dotenv to load environment variables from the .env file
-dotenv.config();
+dotenv.config()
 
-const SECRET_KEY = process.env.JWT_SECRET;
+const SECRET_KEY = process.env.JWT_SECRET
 
 const login = async (req, res) => {
-  console.log("request is here")
+  console.log('request is here')
   try {
-    const { email, password } = req.body;
-    const existingUser = await userModel.findOne({ email });
-    console.log(email);
+    const { email, password } = req.body
+    const existingUser = await User.findOne({ email })
+    console.log(email)
 
     if (!existingUser) {
-      console.log("User not found!");
-      return res.status(404).send("User not found.");
+      console.log('User not found')
+      return res.status(404).send('User not found')
     }
 
-    const matchPassword = await bcrypt.compare(password, existingUser.password);
-    console.log(matchPassword);
+    const matchPassword = await bcrypt.compare(password, existingUser.password)
+    console.log(matchPassword)
 
-    if (!matchPassword)
-      return res.send(400).json({ message: "Invalid Password." });
+    if (!matchPassword) {
+      return res.send(400).json({ message: 'Invalid Password' })
+    }
 
     const token = jwt.sign(
       { email: existingUser.email, id: existingUser._id },
       SECRET_KEY
-    );
+    )
 
-    return res.status(200).json({ user: existingUser, token: token });
+    return res.status(200).json(
+      {
+        user: existingUser,
+        token,
+        message: 'Login successful'
+      }
+    )
   } catch (error) {
-    console.log("Error while logging in User: ", error);
-    return res.status(500).send("Some error occured.");
+    console.log('Error while logging in User: ', error)
+    return res.status(500).send('Some error occured')
   }
-};
+}
 
-export default login;
+export default login
