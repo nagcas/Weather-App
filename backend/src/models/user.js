9b95dbba3038bbea5d7ecf1b726127af16d493d5
nagcas <studio.nagcas@outlook.it>
2025-07-03 02:19:05 +0200

    refactor: improve code formatting and consistency in user.js

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,36 +1,37 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose'
 
 const userSchema = new Schema(
   {
     username: {
       type: String,
       required: true,
+      trim: true
     },
     password: {
       type: String,
-      required: true,
+      required: true
     },
     email: {
       type: String,
       required: true,
       unique: true, // Ensures no duplicate emails
-      match: [/\S+@\S+\.\S+/, 'Please provide a valid email address'], // Email validation
+      match: [/\S+@\S+\.\S+/, 'Please provide a valid email address'] // Email validation
     },
     favoriteCities: [
       {
         type: Schema.Types.ObjectId, // This stores the IDs of the favorite cities
-        ref: "City", // Reference to the City model
+        ref: 'City' // Reference to the City model
         // Each user can have a list of favorite cities, referenced by their IDs
-      },
-    ],
+      }
+    ]
   },
   {
     timestamps: true, // Automatically adds createdAt and updatedAt fields
-    collection: "userModels", // Optional, specifies the collection name
+    versionKey: false,
+    collection: 'users' // Optional, specifies the collection name
   }
-);
+)
 
-const userModel = model("User", userSchema);
+const User = model('users', userSchema)
 
-export default userModel;
-
+export default User
