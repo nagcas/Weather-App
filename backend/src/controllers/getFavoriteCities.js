ad579af514d6095d722ebeb1f2fd46774080eedf
nagcas <studio.nagcas@outlook.it>
2025-07-03 02:18:43 +0200

    refactor: improve code formatting and consistency in getFavoriteCities.js

--- backend/src/controllers/getFavoriteCities.js
+++ backend/src/controllers/getFavoriteCities.js
@@ -1,23 +1,23 @@
-import userModel from "../models/user.js";
+import User from '../models/user.js'
 
 const getFavoriteCities = async (req, res) => {
-  //as this controller will be called after the request has passed auth middleware so the request will contain userid already and we know the user is authenticated
+  // as this controller will be called after the request has passed auth middleware so the request will contain userid already and we  know the user is authenticated
 
-  const { userId } = req;
   try {
-    const user = await userModel.findById(userId);
+    const { userId } = req
+    const user = await User.findById(userId)
 
-    if (!user || !user.favoriteCities || user.favoriteCities.length == 0) {
-      return res.status(200).json({ message: "No favorite cities found." });
+    if (!user || !user.favoriteCities || user.favoriteCities.length === 0) {
+      return res.status(200).json({ message: 'No favorite cities found.' })
     }
 
-    return res.json({ list: user.favoriteCities }).status(200);
+    return res.json({ list: user.favoriteCities }).status(200)
   } catch (error) {
-    console.error("Error occured in getting favorite cities: ", error);
+    console.error('Error occured in getting favorite cities: ', error)
     return res
       .status(500)
-      .json({ message: "Error occured in getting cities." });
+      .json({ message: 'Error occured in getting cities.' })
   }
-};
+}
 
-export default getFavoriteCities;
+export default getFavoriteCities
