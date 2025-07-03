5502ba4d5dc1261742a7d567f4886654a994078c
nagcas <studio.nagcas@outlook.it>
2025-07-03 02:19:01 +0200

    refactor: improve code formatting and consistency in cities.js

--- backend/src/models/cities.js
+++ backend/src/models/cities.js
@@ -1,4 +1,4 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose'
 
 const citySchema = new Schema(
   {
@@ -6,29 +6,28 @@ const citySchema = new Schema(
       type: String,
       required: true,
       unique: true, // Ensures no two cities can have the same name
-      trim: true,   // Removes any leading or trailing whitespace
+      trim: true // Removes any leading or trailing whitespace
     },
     country: {
       type: String,
       required: true, // Corrected from 'require' to 'required'
-      trim: true,     // Removes any leading or trailing whitespace
+      trim: true // Removes any leading or trailing whitespace
     },
     users: [
       {
         type: Schema.Types.ObjectId, // This stores the IDs of users who have this city as a favorite
-        ref: "User", // Reference to the User model
-      },
-    ],
+        ref: 'User' // Reference to the User model
+      }
+    ]
   },
   {
-    timestamps: true, // Automatically adds createdAt and updatedAt fields
-    collection: "cityModels", // Optional: the name of the collection in the database
+    timestamps: true,
+    versionKey: false,
+    collection: 'cities'
   }
-);
+)
 
 // Name the model "City" for better clarity and convention
-const City = model("City", citySchema);
+const City = model('cities', citySchema)
 
-export default City;
-
-
+export default City
